e1ff39a086c816a684b08eb793259d5ddc0a4fc0
Daniel <dwburer@gmail.com>
2016-11-17 13:00:46 -0500

    working dir

--- FileSystem.cpp
+++ FileSystem.cpp
@@ -28,7 +28,6 @@ void FileSystem::list() {
 void FileSystem::changeDirectory(string dir) {
 
 	if(!dir.compare("..")) {
-		cout << "has parent:" << currentDirectory->hasParent << endl;
 		if(currentDirectory->hasParent) {
 			currentDirectory = currentDirectory->getParent();
 		} else {
@@ -58,7 +57,7 @@ void FileSystem::removeDirectory(string dir) {
 			cout << "found directory " << dir << " deleting" << endl;
 			currentDirectory->children.erase(dir);
 		} else {
-			cout << "error: \"" << dir << "\" is a file, not a directory." << endl;
+			cout << "error: \"" << dir << "\" is a file, not a directory" << endl;
 		}
 	} else {
 		cout << dir << ": directory not found" << endl;
@@ -82,7 +81,7 @@ void FileSystem::removeFile(string filename) {
 			cout << "found file " << filename << " deleting" << endl;
 			currentDirectory->children.erase(filename);
 		} else {
-			cout << "error: \"" << filename << "\" is a directory, not a file." << endl;
+			cout << "error: \"" << filename << "\" is a directory, not a file" << endl;
 		}
 	} else {
 		cout << filename << ": file not found" << endl;
@@ -90,11 +89,13 @@ void FileSystem::removeFile(string filename) {
 }
 
 string FileSystem::getWorkingDirectory() {
-	string dir;
+	string dir = currentDirectory->getName();
+	Directory* backtrack = currentDirectory;
 
-	if(currentDirectory->hasParent) {
-
+	while(backtrack->hasParent) {
+		backtrack = backtrack->getParent();
+		dir = backtrack->getName() + "/" + dir;
 	}
 
-	return currentDirectory->getName();
+	return !dir.compare("/") ? dir : dir.substr(1, dir.size());
 }
